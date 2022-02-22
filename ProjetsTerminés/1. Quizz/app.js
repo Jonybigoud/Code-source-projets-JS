21e7daeba5418f6f1b7ad34e308b8c1e8c2a56ed
Schemseddine Larbi <schem.larbi@free.fr>
2022-02-22 22:43:53 +0100

    mise a jour to-do

--- ProjetsTerminés/1. Quizz/app.js
+++ ProjetsTerminés/1. Quizz/app.js
@@ -10,12 +10,12 @@ let verifTableau = [];
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    // console.log(document.querySelector('input[name="q1"]:checked').value);
+    //console.log(document.querySelector('input[name="q1"]:checked').value);
 
     for(i = 1; i < 6; i++) {
         tableauResultats.push(document.querySelector(`input[name="q${i}"]:checked`).value)
     }
-    // console.log(tableauResultats);
+    //console.log(tableauResultats);
     verifFunc(tableauResultats);
     tableauResultats = [];
 })
